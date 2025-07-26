64beb743ff64442555b5826d26c67f961666b9f8
Youssef wael <youssefwael063@gmail.com>
2025-07-26 19:54:47 +0300

    some updates on api

--- backend/src/routes/City.ts
+++ backend/src/routes/City.ts
@@ -1,10 +1,13 @@
-// routes/City.ts
 import express from "express";
 import { createCityHandler, getCitiesHandler } from "../modules/City";
+import { verifyTokenMiddleware } from "../middlewares/auth";
+import { requireAdmin } from "../middlewares/adminauth"; 
 
 const router = express.Router();
 
+router.get('/all', getCitiesHandler);
 
-router.get('/all', getCitiesHandler); // Endpoint to get all cities
-router.post('/create', createCityHandler); // Endpoint to create a new city
+
+router.post('/create', verifyTokenMiddleware, requireAdmin, createCityHandler);
+
 export default router;
