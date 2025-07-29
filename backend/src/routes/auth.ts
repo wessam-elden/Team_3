17f8e046abaebcd2407eacf30aa268892818eaf6
Moaaz Hany <moaazhany330@gmail.com>
2025-07-29 22:08:43 +0300

    Reset Password and OTP

--- backend/src/routes/auth.ts
+++ backend/src/routes/auth.ts
@@ -3,12 +3,20 @@ import { Router } from 'express';
 import { getProfile, login, signup } from '../modules/auth';
 import { verifyTokenMiddleware } from '../middlewares/auth';
 import { createVisitHandler, getAllVisitsHandler } from '../modules/visit';
+import { requestPasswordReset, verifyOtp, resetPasswordWithOtp } from '../modules/passwordReset';
 
 const router = Router();
 
 router.post('/signup', signup);
 
 router.post('/login', login); 
+
+
+
+// Password Reset with OTP
+router.post('/request-password-reset', requestPasswordReset); // Step 1
+router.post('/verify-otp', verifyOtp);                         // Step 2
+router.post('/reset-password', resetPasswordWithOtp);         // Step 3
 
 
 
